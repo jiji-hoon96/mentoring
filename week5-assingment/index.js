e865e78e9cdade75b6499923c2da7db8320efec7
JIHOON LEE <jihoon7705@gmail.com>
2024-05-30 14:41:59 +0900

    feat: Halt event loop when both microtask and macrotask queues are empty

--- week5-assingment/index.js
+++ week5-assingment/index.js
@@ -1,6 +1,8 @@
 let functionIndex = 0;
 let macroIndex = 0;
 let microIndex = 0;
+let microTasks = 0;
+let macroTasks = 0;
 
 const delay = (ms) =>
   new Promise((resolve) => {
@@ -23,6 +25,7 @@ const removeIcon = (createEl) => {
 
 const moveTask = (area, createEl) => {
   const areaEl = document.getElementById(area);
+  const eventLoopIconEl = document.querySelector('#eventLoop span');
   if (!areaEl) return;
 
   if (area === 'stack') {
@@ -37,6 +40,10 @@ const moveTask = (area, createEl) => {
     removeIcon(createEl);
   }
 
+  if (microTasks === 0 && macroTasks === 0) {
+    eventLoopIconEl?.classList.remove('on');
+  }
+
   return delay(1000);
 };
 
@@ -45,11 +52,14 @@ const isTasksInStack = (stackEl) => stackEl && stackEl.childElementCount > 0;
 const isPendingMacroTask = (task, microEl) =>
   task === 'asyncMacroTask' && microEl && microEl.childElementCount > 0;
 
-const processEventLoop = (createEl, eventLoopIconEl) => {
+const processEventLoop = (createEl, eventLoopIconEl, task) => {
+  if (task === 'asyncMacroTask') {
+    macroTasks--;
+  } else {
+    microTasks--;
+  }
   eventLoopIconEl?.classList.add('on');
-  moveTask('stack', createEl)
-    .then(() => eventLoopIconEl?.classList.remove('on'))
-    .then(() => moveTask('console', createEl));
+  moveTask('stack', createEl).then(() => moveTask('console', createEl));
 };
 
 const createTaskElement = (task, innerText) => {
@@ -84,7 +94,7 @@ const asyncTask = (task, createEl) => {
     const loop = setInterval(() => {
       if (isTasksInStack(stackEl) || isPendingMacroTask(task, microEl)) return;
       clearInterval(loop);
-      processEventLoop(createEl, eventLoopIconEl);
+      processEventLoop(createEl, eventLoopIconEl, task);
     }, 0);
   });
 };
@@ -98,6 +108,11 @@ const handleButtonClick = (event) => {
   const createEl = createTaskElement(task, innerText);
   moveTask('stack', createEl).then(() => {
     if (task !== 'syncFunc') {
+      if (task === 'asyncMacroTask') {
+        macroTasks++;
+      } else {
+        microTasks++;
+      }
       handleAsyncTask(task, createEl);
     } else {
       moveTask('console', createEl);
