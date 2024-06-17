bfb83d400614316deba5189acbb24acd93e8209c
JIHOON LEE <jihoon7705@gmail.com>
2024-06-17 14:42:04 +0900

    feat : babel 설정 및 jsx 구문으로 코드 변경

--- week7-assignment/page1.js
+++ week7-assignment/page1.js
@@ -1,54 +1,111 @@
+/** @jsx h */
 import { FORM_DATA } from './common.js';
 
 export default function sectionPage1() {
-  return `
-<header>
-  <div class="bgPurple"></div>
-  <h1>Survey</h1>
-  <strong class="require">* 표시는 필수 질문임</strong>
-</header>
-<form name="form" id="form1" method="post">
-   <div class="box">
-     <fieldset>
-        <legend>radio input<span class="require">*</span></legend>
-        <div class="fieldBox"><label><input type="radio" name="radio" value="radio1" 
-        ${
-          FORM_DATA.radio === 'radio1' ? 'checked' : ''
-        }>radio option1</label></div>
-        <div class="fieldBox"><label><input type="radio" name="radio" value="radio2" 
-        ${
-          FORM_DATA.radio === 'radio2' ? 'checked' : ''
-        }>radio option2</label></div>
-        <div class="fieldBox"><label><input type="radio" name="radio" value="radio3" 
-        ${
-          FORM_DATA.radio === 'radio3' ? 'checked' : ''
-        }>radio option3</label></div>
-     </fieldset> 
-     <small>※ 필수 항목입니다.</small>
-   </div>
-  
-   <div class="box">
-      <fieldset>
-         <legend>checkbox input<span class="require">*</span></legend>
-         <div class="fieldBox"><label><input type="checkbox" name="checkbox" value="checkbox1"
-          ${
-            FORM_DATA.checkbox[0] ? 'checked' : ''
-          }>checkbox option1</label></div>
-         <div class="fieldBox"><label><input type="checkbox" name="checkbox" value="checkbox2"
-          ${
-            FORM_DATA.checkbox[1] ? 'checked' : ''
-          }>checkbox option2</label></div>
-         <div class="fieldBox"><label><input type="checkbox" name="checkbox" value="checkbox3"
-          ${
-            FORM_DATA.checkbox[2] ? 'checked' : ''
-          }>checkbox option3</label></div>
-      </fieldset>
-      <small>※ 필수 항목입니다.</small>
-   </div>
-   <div class="buttons">
-      <button type="button" id="validationBtn" class="next" data-navigate="#/2">다음</button>
-      <button type="button" id="resetBtn" class="reset">양식지우기</button>
-   </div>
-</form>
-`;
+  return (
+    <div>
+      <header>
+        <div className="bgPurple"></div>
+        <h1>Survey</h1>
+        <strong className="require">* 표시는 필수 질문임</strong>
+      </header>
+      <form name="form" id="form1" method="post">
+        <div className="box">
+          <fieldset>
+            <legend>
+              radio input<span className="require">*</span>
+            </legend>
+            <div className="fieldBox">
+              <label>
+                <input
+                  type="radio"
+                  name="radio"
+                  value="radio1"
+                  checked={FORM_DATA.radio === 'radio1'}
+                />
+                radio option1
+              </label>
+            </div>
+            <div className="fieldBox">
+              <label>
+                <input
+                  type="radio"
+                  name="radio"
+                  value="radio2"
+                  checked={FORM_DATA.radio === 'radio2'}
+                />
+                radio option2
+              </label>
+            </div>
+            <div className="fieldBox">
+              <label>
+                <input
+                  type="radio"
+                  name="radio"
+                  value="radio3"
+                  checked={FORM_DATA.radio === 'radio3'}
+                />
+                radio option3
+              </label>
+            </div>
+          </fieldset>
+          <small>※ 필수 항목입니다.</small>
+        </div>
+        <div className="box">
+          <fieldset>
+            <legend>
+              checkbox input<span className="require">*</span>
+            </legend>
+            <div className="fieldBox">
+              <label>
+                <input
+                  type="checkbox"
+                  name="checkbox"
+                  value="checkbox1"
+                  checked={FORM_DATA.checkbox.includes('checkbox1')}
+                />
+                checkbox option1
+              </label>
+            </div>
+            <div className="fieldBox">
+              <label>
+                <input
+                  type="checkbox"
+                  name="checkbox"
+                  value="checkbox2"
+                  checked={FORM_DATA.checkbox.includes('checkbox2')}
+                />
+                checkbox option2
+              </label>
+            </div>
+            <div className="fieldBox">
+              <label>
+                <input
+                  type="checkbox"
+                  name="checkbox"
+                  value="checkbox3"
+                  checked={FORM_DATA.checkbox.includes('checkbox3')}
+                />
+                checkbox option3
+              </label>
+            </div>
+          </fieldset>
+          <small>※ 필수 항목입니다.</small>
+        </div>
+        <div className="buttons">
+          <button
+            type="button"
+            id="validationBtn"
+            className="next"
+            data-navigate="#/2"
+          >
+            다음
+          </button>
+          <button type="button" id="resetBtn" className="reset">
+            양식지우기
+          </button>
+        </div>
+      </form>
+    </div>
+  );
 }
--- week7-assignment/page3.js
+++ week7-assignment/page3.js
@@ -1,15 +1,19 @@
 import { FORM_DATA } from './common.js';
 
 export default function sectionPage3() {
-  return `
-   <header>
-      <div class="bgPurple"></div>
-      <h1>Survey</h1>
-      <div class="result">
-         응답이 기록되었습니다.<br>
-         ${JSON.stringify(FORM_DATA)}
-      </div>
-      <button class="first" id="newFormBtn">다른 응답 제출</ㅠ>
-   </header>
-   `;
+  return (
+    <div>
+      <header>
+        <div class="bgPurple"></div>
+        <h1>Survey</h1>
+        <div class="result">
+          응답이 기록되었습니다.
+          <br />${JSON.stringify(FORM_DATA)}
+        </div>
+        <button class="first" id="newFormBtn">
+          다른 응답 제출
+        </button>
+      </header>
+    </div>
+  );
 }
